963a44f70892e02c6872768a2e487b082df4f882
xmaestro <alexeyfedorov101@gmail.com>
2016-08-24 13:05:18 +0500

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# ng2-tagsinput
+# Angular 2 : Typescript component for Tags Input
+
+An improved version of [angular2-tag-input](https://github.com/rosslavery/angular2-tag-input)
+
+Installation
+--------------------------------------
+
+Install it from npm:
+
+```bash
+npm install ng2-tagsinput
+```
+
+Usage
+--------------------------------------
+
+### SystemJS config
+
+```js
+System.config({
+    ...
+    map: {
+      ...
+      'ng2-tagsinput': 'node_modules/ng2-tagsinput',
+      ...
+    }
+    ...
+});
+```
+
+### Component
+
+```typescript
+import {Ng2TagsInput} from 'ng2-tagsinput/ng2-tagsinput';
+```
+
+```typescript
+ ...
+@Component({
+  directives: [Ng2TagsInput]
+  })
+  ...
+```
+
+### View
+
+Use in template like below
+
+```html
+<tag-input
+placeholder="Add tags ..."
+[model]="tagsArray"
+(tagsChanged)="onTagsChange($event)"
+(tagsAdded)="onTagsAdded($event)"
+(tasRemoved)="onTagRemoved($event)"
+>
+```
+
+Where **model** is the array of elements which will have the resulting tags list. **tagsChanged** is fired whenever tags list changes i.e. tags are added or removed.
+
+## Inputs
+
+- `model` : `string[]` - Model you want the tags list to bind to.
+- `placeholder` : `string` - **Default**: ``'Add a tag'`` - Placeholder for the `<input>` tag.
+- `delimiterCode` : `Array<number>` - **Default**: ``'[188]'`` - Array of ASCII keycodes to split tags on. Defaults to comma.
+- `addOnBlur` : `boolean` - **Default**: `true` - Whether to attempt to add a tag when the input loses focus.
+- `addOnEnter` : `boolean` - **Default**: `true` - Whether to attempt to add a tag when the user presses enter.
+- `addOnPaste` : `boolean` - **Default**: `true` - Whether to attempt to add a tags when the user pastes their clipboard contents.
+- `allowedTagsPattern` : `RegExp` - **Default**: `/.+/` - RegExp that must match for a tag to be added.
+
+## Outputs(Events)
+
+- `tagsChanged` : `string[]` - Fires when tags list is changed in any way.
+- `tagsAdded` : `string[]` - Fires when tags list is changed in any way.
---
+++ lib/ng2-tagsinput.ts
@@ -0,0 +1,162 @@
+import {Component, HostBinding, Input, EventEmitter, Output} from '@angular/core';
+import {Ng2TagsInputItem} from './ng2-tagsinput-item';
+
+@Component({
+  selector: 'tag-input',
+  template:
+  `<tag-input-item
+    [text]="tag"
+    [index]="index"
+    [selected]="selectedTag === index"
+    (tagRemoved)="_removeTag($event)"
+    *ngFor="let tag of tagsList; let index = index">
+  </tag-input-item>
+  <input
+    class="ng2-tag-input-field"
+    type="text"
+    [placeholder]="placeholder"
+    [(ngModel)]="inputValue"
+    (paste)="inputPaste($event)"
+    (keydown)="inputChanged($event)"
+    (blur)="inputBlurred($event)"
+    (focus)="inputFocused()"
+    #tagInputRef>`,
+  styles: [`
+    :host {
+      display: block;
+      box-shadow: 0 1px #ccc;
+      padding: 5px 0;
+    }
+    :host.ng2-tag-input-focus {
+      box-shadow: 0 2px #0d8bff;
+    }
+    .ng2-tag-input-field {
+      display: inline-block;
+      width: auto;
+      box-shadow: none;
+      border: 0;
+    }
+  `],
+  directives: [Ng2TagsInputItem]
+})
+
+export class Ng2TagsInput {
+
+    @Input() placeholder: string = 'Add a tag';
+    @Input() model: string[];
+    @Input() delimiterCode: string = '188';
+    @Input() addOnBlur: boolean = true;
+    @Input() addOnEnter: boolean = true;
+    @Input() addOnPaste: boolean = true;
+    @Input() allowedTagsPattern: RegExp = /.+/;
+    @HostBinding('class.ng2-tag-input-focus') isFocussed;
+
+    @Output() tagsChanged:EventEmitter<any> = null;
+
+    @Output() tagsAdded:EventEmitter<any> = null;
+
+    @Output() tagRemoved:EventEmitter<any> = null;
+
+    public tagsList:string[] = [];
+    public inputValue:string = '';
+    public delimiter:number;
+    public selectedTag:number;
+
+    constructor() {
+        this.tagsChanged = new EventEmitter<any>();
+        this.tagsAdded = new EventEmitter<any>();
+        this.tagRemoved = new EventEmitter<any>();
+
+    }
+
+    ngOnInit() {
+        if (this.model) this.tagsList = this.model;
+        this.tagsChanged.emit(this.tagsList);
+        this.delimiter = parseInt(this.delimiterCode);
+    }
+
+    inputChanged(event) {
+        let key = event.keyCode;
+        switch (key) {
+            case 8: // Backspace
+                this._handleBackspace();
+                break;
+            case 13: //Enter
+                this.addOnEnter && this._addTags([this.inputValue]);
+                event.preventDefault();
+                break;
+
+            case this.delimiter:
+                this._addTags([this.inputValue]);
+                event.preventDefault();
+                break;
+
+            default:
+                this._resetSelected();
+                break;
+        }
+    }
+
+    inputBlurred(event) {
+        this.addOnBlur && this._addTags([this.inputValue]);
+        this.isFocussed = false;
+    }
+
+    inputFocused(event) {
+        this.isFocussed = true;
+    }
+
+    inputPaste(event) {
+        let clipboardData = event.clipboardData || (event.originalEvent && event.originalEvent.clipboardData);
+        let pastedString = clipboardData.getData('text/plain');
+        let tags = this._splitString(pastedString);
+        let tagsToAdd = tags.filter((tag) => this._isTagValid(tag));
+        this._addTags(tagsToAdd);
+        setTimeout(() => this.inputValue = '', 3000);
+    }
+
+    private _splitString(tagString:string) {
+        tagString = tagString.trim();
+        let tags = tagString.split(String.fromCharCode(this.delimiter));
+        return tags.filter((tag) => !!tag);
+    }
+
+    private _isTagValid(tagString:string) {
+        return this.allowedTagsPattern.test(tagString);
+    }
+
+    private _addTags(tags:string[]) {
+        let validTags = tags.filter((tag) => this._isTagValid(tag));
+        this.tagsList = this.tagsList.concat(validTags);
+        this._resetSelected();
+        this._resetInput();
+        this.tagsAdded.emit(validTags);
+        this.tagsChanged.emit(this.tagsList);
+    }
+
+    private _removeTag(tagIndexToRemove) {
+        this.tagRemoved.emit(this.tagsList[tagIndexToRemove]);
+        this.tagsList.splice(tagIndexToRemove, 1);
+        this._resetSelected();
+        this.tagsChanged.emit(this.tagsList);
+    }
+
+    private _handleBackspace() {
+        if (!this.inputValue.length && this.tagsList.length) {
+            if (this.selectedTag) {
+                this.selectedTag = this.tagsList.length - 1;
+            } else {
+                this._removeTag(this.selectedTag);
+            }
+        }
+    }
+
+    private _resetSelected() {
+        this.selectedTag = null;
+    }
+
+    private _resetInput() {
+        this.inputValue = '';
+    }
+
+}
